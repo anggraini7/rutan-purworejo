ae0ddedba06b636cb980d55bcae6a4430a72e25b
ardhian26 <dwiardhi123@gmail.com>
2026-06-21 13:57:12 +0700

    update url

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/admin/posts');
+        // UBAH BARIS INI: ditambahkan /create di ujungnya agar langsung ke form tambah
+        return redirect()->intended('/admin/posts/create');
     }
 
     /**
@@ -42,6 +43,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }
